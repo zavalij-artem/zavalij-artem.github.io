a3d94e9223803a0029781c7ea3dc5290d2bc38e1
zavalij-artem <zavartema@gmail.com>
2025-03-18 16:17:27 +0200

    Global js fixes
    js rewriting
    style restyling

--- school/js/main.js
+++ school/js/main.js
@@ -1,105 +1,83 @@
-const a = document.getElementById('number-a')
-const b = document.getElementById('number-b')
+let aNumber = document.getElementById('number-a')
 let action = document.getElementById('action')
-const inputAnswer = document.getElementById('answer')
-const btnCheck = document.getElementById('check')
-const btnRefresh = document.getElementById('refresh')
-const resultElement = document.getElementById('result')
+let bNumber = document.getElementById('number-b')
+let checkBtn = document.getElementById('check')
+let createBtn = document.getElementById('create')
+let inputAnswer = document.getElementById('answer')
+let aNumberResult = document.getElementById('number-a-result')
+let actionResult = document.getElementById('action-result')
+let bNumberResult = document.getElementById('number-b-result')
+let rightAnswerResult = document.getElementById('right-answer')
+let resultError = document.getElementById('result-error')
+let resultSuccess = document.getElementById('result-success')
 
 function getRandomIntInclusive(min, max) {
 	const minCeiled = Math.ceil(min);
 	const maxFloored = Math.floor(max);
 	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
-
 let aMin = 1
 let aMax = 10
-
 let bMin = 1
 let bMax = 10
-
-const aRandom = getRandomIntInclusive(aMin, aMax)
-const bRandom = getRandomIntInclusive(bMin, bMax)
-
-const arrayAction = ['+', '-']
-const randomAction = Math.round(Math.random() * (arrayAction.length - 1))
-
-a.textContent = aRandom
-b.textContent = bRandom
-action.textContent = arrayAction[randomAction]
-let actionSymbol = action.textContent
-
-if (aRandom < bRandom && actionSymbol === '-'){
-	console.log('actionSymbol minus overwrited to plus')
-	actionSymbol = '+'
-	action.textContent = '+'
+// generated random values
+function generatedValues(){
+	let aRandom = getRandomIntInclusive(aMin, aMax)
+	let bRandom = getRandomIntInclusive(bMin, bMax)
+	aNumber.innerText = aRandom
+	bNumber.innerText = bRandom
+	aNumberResult.innerText = aRandom
+	bNumberResult.innerText = bRandom
+	// random plus/minus
+	const arrayAction = ['+', '-']
+	const randomAction = Math.round(Math.random() * (arrayAction.length - 1))
+	actionSymbol = arrayAction[randomAction]
+	if (aNumber.innerText < bNumber.innerText && actionSymbol === '-'){
+		console.log('actionSymbol Minus overwrited to Plus')
+		actionSymbol = '+'
+	}
+	action.innerText = actionSymbol
+	actionResult.innerText = actionSymbol
+	let userAnswer = inputAnswer.value
+	let rightAnswer = aRandom + bRandom
+	if (actionSymbol === '-'){
+		rightAnswer = aRandom - bRandom
+	}
+	rightAnswerResult.innerHTML = rightAnswer
 }
+generatedValues()
 
-btnCheck.onclick = function(){
-	const num1 = Number(a.textContent)
-	const num2 = Number(b.textContent)
-	if (actionSymbol == '+'){
-		rightAnswer = num1 + num2
-	} else if (actionSymbol == '-'){
-		rightAnswer = num1 - num2
-	}
-	const userAnswer = inputAnswer.value
-	if(userAnswer == rightAnswer){
-		resultElement.innerHTML = 
-`<span class="d-block">Вірно!</span>
-<span class="d-block fw-bold">${num1} ${actionSymbol} ${num2} = ${rightAnswer}</span>
-`
-		resultElement.classList.add('text-success')
-		resultElement.classList.remove('text-danger')
-		btnRefresh.classList.remove('invisible')
-	} else if(userAnswer == ''){
-		resultElement.textContent = 'Введи, будь-ласка, відповідь!'
-		resultElement.classList.add('text-danger')
-		resultElement.classList.remove('text-success')
-		btnRefresh.classList.add('invisible')
-	}
-	 else {
-		resultElement.textContent = 'Перерахуй, будь-ласка!'
-		resultElement.classList.add('text-danger')
-		resultElement.classList.remove('text-success')
-		btnRefresh.classList.add('invisible')
+function checkAnswer(){
+	if (inputAnswer.value == ''){
+		resultError.innerText = `Введи, будь-ласка, відповідь!`
+		resultSuccess.classList.add('invisible')
+		createBtn.classList.add('invisible')
+	} else if (inputAnswer.value == rightAnswerResult.innerHTML){
+		resultSuccess.classList.remove('invisible')
+		createBtn.classList.remove('invisible')
+		resultError.innerText = ''
+		
+	} else {
+		resultSuccess.classList.add('invisible')
+		createBtn.classList.add('invisible')
+		resultError.innerText = `Перерахуй, будь-ласка!`
 	}
 }
 
-btnRefresh.onclick = function(){
-	location.reload();
+checkBtn.onclick = function(event){
+	checkAnswer()
 }
 
 document.addEventListener('keydown', event => {
 	if(event.code === 'Enter' | event.code === 'NumpadEnter') {
-		const num1 = Number(a.textContent)
-	const num2 = Number(b.textContent)
-	if (actionSymbol == '+'){
-		rightAnswer = num1 + num2
-	} else if (actionSymbol == '-'){
-		rightAnswer = num1 - num2
-	}
-	const userAnswer = inputAnswer.value
-	if(userAnswer == rightAnswer){
-		resultElement.innerHTML = 
-`<span class="d-block">Вірно!</span>
-<span class="d-block fw-bold">${num1} ${actionSymbol} ${num2} = ${rightAnswer}</span>
-`
-		resultElement.classList.add('text-success')
-		resultElement.classList.remove('text-danger')
-		btnRefresh.classList.remove('invisible')
-	} else if(userAnswer == ''){
-		resultElement.textContent = 'Введи, будь-ласка, відповідь!'
-		resultElement.classList.add('text-danger')
-		resultElement.classList.remove('text-success')
-		btnRefresh.classList.add('invisible')
-	}
-	 else {
-		resultElement.textContent = 'Перерахуй, будь-ласка!'
-		resultElement.classList.add('text-danger')
-		resultElement.classList.remove('text-success')
-		btnRefresh.classList.add('invisible')
-	}
+		checkAnswer()
 	}
 })
 
+createBtn.onclick = function(event){
+	resultSuccess.classList.add('invisible')
+	createBtn.classList.add('invisible')
+	resultError.innerText = ''
+	inputAnswer.value = ''
+	generatedValues()
+}
